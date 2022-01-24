b234e5cb34d8626dc9e186e6bb218cd89e5ef686
Abhi <agbhalani11@gmail.com>
2022-01-24 17:33:15 +0530

    Add PostgreSQL and psycopg2

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+from secret import authenticate, identity
+from Resources.user import UserRegister
+from Resources.item import Item, ItemList
+from Resources.store import Store, StoreList
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
+uri = app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
@@ -37,4 +40,3 @@ if __name__ == '__main__':
             db.create_all()
 
     app.run(port=5000)
-    
