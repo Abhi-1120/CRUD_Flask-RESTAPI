8c53fc6fbece51d80b0fd39c88011ac1c10dcee6
Abhi <agbhalani11@gmail.com>
2022-01-25 09:43:33 +0530

    change in app.py and uwsgi.ini

--- app.py
+++ app.py
@@ -33,8 +33,6 @@ if __name__ == '__main__':
     db.init_app(app)
 
     if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+        from db import db
+        db.init_app(app)
+        app.run(port=5000)
